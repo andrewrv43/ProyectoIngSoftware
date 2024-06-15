745d5c9fde0b441aed52bcfdaa2bf01ecfcd9a8b
Andrew <paul.rueda@hotmail.com>
2024-06-14 20:58:49 -0500

    Nueva Base de datos y creacion de tablas

--- app.py
+++ app.py
@@ -11,8 +11,13 @@ from controlador.promociones_control import promociones_blueprint
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
+
 db.init_app(app)
 swagger = Swagger(app)
+
+with app.app_context():
+    db.create_all()
 
 app.register_blueprint(usuario_blueprint)
 app.register_blueprint(producto_blueprint)
