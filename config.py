853693ffeb7e755bb0e45b71fda45810111efdfe
Andrew <paul.rueda@hotmail.com>
2024-06-14 23:42:49 -0500

    Last Config de swagger

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SWAGGER = {
+        'title': 'API PARA LA GESTION DE MICROMERCADO',
+        'description': 'Aqui definimos la gestion de usuarios roles, creacion de productos informacion de proveedores y más.'
+    }
